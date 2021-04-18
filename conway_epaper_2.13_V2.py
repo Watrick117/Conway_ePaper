6f58c9c99015ef6a840d856bc79511ceb8a2b012
Watrick117 <60557071+Watrick117@users.noreply.github.com>
2021-04-18 18:07:42 -0400

    Update conway_epaper_2.13_V2.py

--- conway_epaper_2.13_V2.py
+++ conway_epaper_2.13_V2.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python
-# -*- coding:utf-8 -*-
 import sys
 import os
-picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
-libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+
+picdir = os.path.join(os.path.dirname(os.path.dirname(
+         os.path.realpath(__file__))), 'pic')
+
+libdir = os.path.join(os.path.dirname(os.path.dirname(
+         os.path.realpath(__file__))), 'lib')
+
 if os.path.exists(libdir):
     sys.path.append(libdir)
 
-import logging
 from waveshare_epd import epd2in13_V2
 import time
-from PIL import Image,ImageDraw,ImageFont,ImageOps
+from PIL import Image, ImageDraw, ImageFont
 import traceback
 
 import argparse
@@ -19,25 +22,34 @@ import numpy as np
 from datetime import datetime
 import png
 
-print("Im working")
-time.sleep(30)
-
 my_parser = argparse.ArgumentParser()
+my_parser.add_argument('-s', '--size', action='store', type=int, nargs=2,
+                       help="Takes in two arguments that" +
+                       "represent the size of the board.")
 my_parser.add_argument('-g', '--generation', action='store', type=int,
-    help="Sets the max generation.")
-my_parser.add_argument('-p', '--population', action='store', type=int,
-    help="Percentage of times that random cells of life are added to based on board size.")
-my_parser.add_argument('-s', '--sleep', action='store', type=int,
-    help="Time that the program sleeps inbetween generations")
+                       help="Sets the max generation.")
+my_parser.add_argument('-p', '--population', action='store',
+                       type=int, help="Percentage of times that random" +
+                       "cells of life are added to based on board size.")
+
 my_parser.add_argument('-d', '--delete', action='store_true',
-    help="Flag used if you want the images of generations deleted when program is done running.")
+                       help="Flag used if you want the images of generations" +
+                       "deleted when program is done running.")
 
 # Sanity checks user inputs
 args = my_parser.parse_args()
 
 # Fills in default values if user did not define
-height = 250
-width = 122
+if args.size is None:
+    HGT = 250
+    WID = 122
+else:
+    if args.size[0] > 9 and args.size[1] > 9:
+        HGT = int(args.size[0])
+        WID = int(args.size[1])
+    else:
+        print(f'ERROR: Minimum board size is 10 by 10')
+        sys.exit(0)
 
 if args.generation is None:
     maxgeneration = 250
@@ -49,34 +61,20 @@ if args.population is None:
 else:
     population = args.population / 100
 
-if args.sleep is None:
-    sleep = 2
-else:
-    sleep = args.sleep
-
-logging.basicConfig(level=logging.DEBUG)
-
 path = os.getcwd()
-isdir = os.path.isdir("temp")
 np.random.seed(random.seed(datetime.now()))
 
 # Generation 0
 generation = 0
-newtable = np.zeros((width,height))
-oldtable = newtable
+newgen = np.zeros((WID, HGT))
+oldgen = newgen
 
-# Generation 1
-# Randomly sets an intial state of living squares
-for i in range(int(width * height * (population))):
-    newtable[random.randint(0, width - 1 )][random.randint(0, height - 1 )] = 1
 
-print(newtable)
-
-def countNeighbors(oldtable,x,y):
+def countNeighbors(oldgen, x, y):
     """Counts the living neighbors of a cell
 
     Args:
-        oldtable: 
+        oldgen:
         x (int): cordanante of the starting cell
         y (int): coordinate of the starting cell
 
@@ -87,105 +85,53 @@ def countNeighbors(oldtable,x,y):
     temp = 1
 
     count = 0
-    for i in range(-1,2):
-        for j in range(-1,2):
-            if not (i==0 and j==0):  #TODO: this needs rewritin to be more understandable
-                count += int(oldtable[(x + i + width) % width][(y + j + height) % height])
+    for i in range(-1, 2):
+        for j in range(-1, 2):
 
-    for i in range(-1,2):
-        for j in range(-1,2):
+            # TODO: this needs rewritin to be more understandable
+            if not (i == 0 and j == 0):
+                count += int(oldgen[(x + i + WID) % WID][(y + j + HGT) % HGT])
+
+    for i in range(-1, 2):
+        for j in range(-1, 2):
             temp += 1
 
-    count -= int(oldtable[x][y])
+    count -= int(oldgen[x][y])
 
     return count
 
-if isdir == False:
-    os.mkdir("temp")
-
 epd = epd2in13_V2.EPD()
-logging.info("init and Clear")
 epd.init(epd.FULL_UPDATE)
-
-logging.info("Conways Game of life on wave share 2.13in e-Paper")
 
 while True:
-    # epd.Clear(0xFF)
-    logging.info("Clear Screen")
-    #epd.init(epd.FULL_UPDATE)
-    #epd.Clear(0xFF)
 
-    oldtable = newtable
+    # Generation 1 Creation
+    # Randomly sets an intial state of living squares
+    for i in range(int(WID * HGT * (population))):
+        newgen[random.randint(0, WID - 1)][random.randint(0, HGT - 1)] = 1
 
-    #np.invert(newtable)
-    binary_transform = np.array(newtable).astype(np.uint8)
-    binary_transform[binary_transform>0] = 255    
-    img = Image.fromarray(binary_transform, 'P')
-    img.save('image.bmp')
+    while True:
 
-    image = Image.open(os.path.join("image.bmp"))
-    epd.display(epd.getbuffer(image))
+        oldgen = newgen
 
-    # Calculates the state of the next generation
-    for x in range(width):
-        for y in range(height):
-            neighbors =  countNeighbors(oldtable,x,y)
-            # Calculate dead cells
-            if oldtable[x][y] == 0 and neighbors == 3:
-                newtable[x][y] = 1
-            # Calculate alive cells
-            elif oldtable[x][y] == 1 and (neighbors < 2 or neighbors > 3):
-                newtable[x][y] = 0
+        # Converts newgen array into an image object
+        binary_transform = np.array(newgen).astype(np.uint8)
+        binary_transform[binary_transform > 0] = 255
+        img = Image.fromarray(binary_transform, 'L')
+        epd.display(epd.getbuffer(img))
 
-    #print("Generation: ", generation)
-    generation +=1
+        # Calculates the state of the next generation
+        for x in range(WID):
+            for y in range(HGT):
+                neighbors = countNeighbors(oldgen, x, y)
+                # Calculate dead cells
+                if oldgen[x][y] == 0 and neighbors == 3:
+                    newgen[x][y] = 1
+                # Calculate alive cells
+                elif oldgen[x][y] == 1 and (neighbors < 2 or neighbors > 3):
+                    newgen[x][y] = 0
 
-    if generation > maxgeneration:
-        break
+        generation += 1
 
-    #time.sleep(2)
-
-"""
-try:
-    
-    epd = epd2in13_V2.EPD()
-    logging.info("init and Clear")
-    epd.init(epd.FULL_UPDATE)
-    epd.Clear(0xFF)
-    
-    # read bmp file 
-    logging.info("2.read bmp file...")
-    image = Image.open(os.path.join(picdir, '2in13.bmp'))
-    epd.display(epd.getbuffer(image))
-    time.sleep(2)
-"""
-
-"""
-    # read bmp file on window
-    logging.info("3.read bmp file on window...")
-    # epd.Clear(0xFF)
-    image1 = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
-    bmp = Image.open(os.path.join(picdir, '100x100.bmp'))
-    image1.paste(bmp, (2,2))    
-    epd.display(epd.getbuffer(image1))
-    time.sleep(2)
-"""
-    
-# epd.Clear(0xFF)
-logging.info("Clear...")
-epd.init(epd.FULL_UPDATE)
-epd.Clear(0xFF)
-
-logging.info("Goto Sleep...")
-epd.sleep()
-        
-"""
-except IOError as e:
-    logging.info(e)
-
-    
-except KeyboardInterrupt:    
-    logging.info("ctrl + c:")
-    epd2in13_V2.epdconfig.module_exit()
-    exit()
-"""
+        if generation > maxgeneration:
+            break
